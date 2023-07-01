fd5619cad107e93aeeb30e0f427e38fc4900db89
Sharon Nyarinda <nyarindas@gmail.com>
2023-07-01 11:06:50 +0300

    added Hero getter and setters

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,12 @@
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
 }
